0a9d606f519a65c9bc452c54759012d6d700a547
KIDJourney <kingdeadfish@qq.com>
2016-11-16 18:37:08 +0800

    fix result bar

--- berserker/result.py
+++ berserker/result.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 
 
 class ProcessBar:
-    def __init__(self, goal, fill='=', blank='-', output_format='[{}>{}] {}%'):
+    def __init__(self, goal, fill='=', blank='-', output_format='[{}>{}] {:.0f}%', width=12):
         self.goal = goal
         self.current = 0
         self.fill = fill
@@ -12,23 +12,27 @@ class ProcessBar:
         self.format = output_format
 
     def _current_process(self):
-        return 100 * self.current // self.goal
+        return self.current / self.goal
 
     def _finished(self):
-        return self._current_process() * self.get_width() * self.fill
+        return int(self._current_process() * self.get_width())
 
     def _blanked(self):
-        return (100 - self._current_process()) * self.get_width() * self.blank
+        return self.get_width() - self._finished()
 
     def get_width(self):
         if hasattr(sys.stdout, 'istty') and sys.stdout.istty():
-            return int(os.popen('stty size', 'r').read().split()[1]) // 200
+            return int(os.popen('stty size', 'r').read().split()[1])
         else:
-            return 1
+            return 30
+
+    def get_step(self):
+        return self.goal // self.get_width()
 
     def show(self):
         sys.stdout.write('\r')
-        output = self.format.format(self._finished(), self._blanked(), self._current_process())
+        output = self.format.format(self._finished() * self.fill, self._blanked() * self.blank,
+                                    self._current_process() * 100)
         sys.stdout.write(output)
         sys.stdout.flush()
 
@@ -66,13 +70,11 @@ class Results:
     def add_status_record(self, response, duration):
         self.responses.append(response)
         self.status_counter[response.status_code].append(duration)
+        self.html_transferred += len(response.content)
         self.incr()
 
     def set_total_time(self, total_time):
         self.total_time = total_time
-
-    def parse_response(self, response):
-        self.html_transferred += len(response.content)
 
     def incr(self):
         self.process_bar.incr()
@@ -81,12 +83,13 @@ class Results:
         result = {}
 
         result['concurrency'] = self.concurrency
+        result['request_nums'] = self.total_request
         result['time_taken_for_tests'] = self.total_time
         result['complete_requests'] = sum(map(len, self.status_counter.values()))
         result['failed_requests'] = len(self.errors)
         result['html_transferred'] = self.html_transferred
         result['request_per_second'] = self.total_request / self.total_time
-        result['time_per_request'] = result['complete_requests'] / self.total_time
+        result['time_per_request'] = self.total_time / self.total_request
         result['transfer_rate'] = self.html_transferred / self.total_time / 1024
 
         request_durations = [duration for status_code, duration_list in self.status_counter.items() for duration in
@@ -104,20 +107,22 @@ class Results:
     def show(self):
         result = self.cal_status()
         output = """
+
 Concurrency Level:      {concurrency}
-Time taken for tests:   {time_taken_for_tests} seconds
+Request Level:          {request_nums}
+Time taken for tests:   {time_taken_for_tests:.2f} seconds
 Complete requests:      {complete_requests}
 Failed requests:        {failed_requests}
 HTML transferred:       {html_transferred} bytes
-Requests per second:    {request_per_second} [#/sec] (mean)
-Time per request:       {time_per_request} [ms] (mean)
-Transfer rate:          {transfer_rate} [Kbytes/sec] received
+Requests per second:    {request_per_second:.2f} [#/sec] (mean)
+Time per request:       {time_per_request:.2f} [ms] (mean)
+Transfer rate:          {transfer_rate:.2f} [Kbytes/sec] received
 """.format(**result)
         output += """
 Percentage of the requests served within a certain time (ms)
 """
-        for index, value in enumerate(result['duration_distribution']):
-            output += '{}% {} ms\n'.format(50 + 10 * index, value)
+
+        print(output)
 
 
 if __name__ == "__main__":
