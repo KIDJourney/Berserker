a5b537d9e1b80282097c557549b91441377cd95a
KIDJourney <kingdeadfish@qq.com>
2016-11-16 21:25:40 +0800

    add setup tool

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from berserker import version
+
+install_requires = ['gevent', 'requests>=2.3.0']
+
+description = 'Web Application Smoking Test'
+
+classifiers = ["License :: OSI Approved :: MIT",
+               "Programming Language :: Python :: 3.3",
+               "Programming Language :: Python :: 3.4",
+               "Programming Language :: Python :: 3.5",
+               "Programming Language :: Python :: 3.6"]
+
+setup(name='berserker',
+      version=version,
+      url='https://github.com/KIDJourney/Berserker',
+      packages=find_packages(),
+      long_description=description,
+      author="KIDJourney",
+      author_email="kingdeadfish@qq.com",
+      classifiers=classifiers,
+      install_requires=install_requires,
+      entry_points="""
+      [console_scripts]
+      berserker = berserker.core:main
+      """)
